c73d52cf97270fc9f3c212b3dbcc385bb8fbc610
wpted <silverhandsome@gmail.com>
2022-07-05 22:09:07 +0800

    set http header due to CORS

--- controller/dareController.go
+++ controller/dareController.go
@@ -21,46 +21,67 @@ var MockDareList = dareList{
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	// message is a one-time struct only appears here
 	message := struct {
 		Message string `json:"Message"`
 	}{"welcome to drunk dares"}
-	js, err := json.Marshal(message)
+
+	// Marshal returns a jsonified message as []byte and an error
+	output, err := json.Marshal(message)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
+	// Content-Type is used to indicate the media type of the resource
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+
+	// Header set for CORS issue
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	// write output []bytes to writer interface, here the http.ResponseWriter
+	w.Write(output)
 
 }
 
 // getRandomIndex is a helper function getting a random index within [0, len(dareList))
 func getRandomIndex(d dareList) int {
+	// set seed related accordingly to the current time
 	rand.Seed(time.Now().Unix())
 	return rand.Intn(len(d))
 }
 
+// GetRandomDare is a dareList method that returns a random jsonified dare
 func (d *dareList) GetRandomDare(w http.ResponseWriter, r *http.Request) {
 
 	randomDare := (*d)[getRandomIndex(*d)].DareQuestion
 
-	js, err := json.Marshal(randomDare)
+	output, err := json.Marshal(randomDare)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	w.Write(output)
 }
 
+// GetAllDare is a dareList method that returns all jsonified dares
 func (d *dareList) GetAllDare(w http.ResponseWriter, r *http.Request) {
-	js, err := json.Marshal(*d)
+	output, err := json.Marshal(*d)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	w.Write(output)
 }
