1b9e94d0892002fc88f0704cbd12d087df65d3a1
wpted <silverhandsome@gmail.com>
2022-07-09 22:55:07 +0800

    add a new handler function for CreateDare

--- app/app.go
+++ app/app.go
@@ -9,6 +9,10 @@ func Run() {
 	http.HandleFunc("/", controller.Home)
 	http.HandleFunc("/RandomDare", controller.MockDareList.GetRandomDare)
 	http.HandleFunc("/Dares", controller.MockDareList.GetAllDare)
+	// if we want to use same endpoint names with different http methods, we have to create a different mux (multiplexer)
+	// so here we use different endpoint names, and should modify in the future
+	http.HandleFunc("/CreateDare", controller.CreateDare)
+	//http.HandleFunc("/UpdateDare", controller.UpdateDare)
 
 	http.ListenAndServe(":8080", nil)
 }
