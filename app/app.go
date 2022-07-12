3aa51af61ca46449c3b062c3581b7ff5fcad422a
wpted <silverhandsome@gmail.com>
2022-07-12 09:02:26 +0800

    Load .env file, connect to database and migrate the database with the model.Dare struct.

--- app/app.go
+++ app/app.go
@@ -1,11 +1,40 @@
 package app
 
 import (
+	"dareAPI/configs"
 	"dareAPI/controller"
+	"dareAPI/model"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
+	"log"
 	"net/http"
 )
 
+// Read the .env file first
+func init() {
+	godotenv.Load()
+}
+
 func Run() {
+	// Connect to database
+	postgresConfig := configs.GetPostgresConfig()
+	db, err := gorm.Open(postgresConfig.Dialect(), postgresConfig.GetConnectionURI())
+	fmt.Println(postgresConfig.GetConnectionURI())
+	if err != nil {
+		log.Panic(err)
+	} else {
+		fmt.Println("Successfully connect to database!")
+	}
+
+	// Migration of structs and the database
+	// This should be done only once
+	db.AutoMigrate(&model.Dare{})
+	defer db.Close()
+
+	// Router handling the endpoints
 	http.HandleFunc("/", controller.Home)
 	http.HandleFunc("/RandomDare", controller.MockDareList.GetRandomDare)
 	http.HandleFunc("/Dares", controller.MockDareList.GetAllDare)
