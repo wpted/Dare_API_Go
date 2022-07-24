99c731aafda3b5536bc5406923af39c03060f5b3
wpted <silverhandsome@gmail.com>
2022-07-24 21:45:52 +0800

    Alter the ID tag

--- model/dare.go
+++ model/dare.go
@@ -1,12 +1,15 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Message struct {
 	Message string `json:"Message"`
 }
+
 type Dare struct {
 	// Model is a struct having default fields where ID is a primary key
-	ID   string
-	Dare string `json:"dare" "`
+	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Dare string             `json:"dare,omitempty" bson:"dare,omitempty"`
 }
 
 type DareContainer []Dare
