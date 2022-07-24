d1620d536e7e4876df76d864358b834048a11033
wpted <silverhandsome@gmail.com>
2022-07-24 23:24:08 +0800

    Delete Message struct

--- model/dare.go
+++ model/dare.go
@@ -2,14 +2,10 @@ package model
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type Message struct {
-	Message string `json:"Message"`
-}
-
 type Dare struct {
 	// Model is a struct having default fields where ID is a primary key
-	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Dare string             `json:"dare,omitempty" bson:"dare,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Question string             `json:"question,omitempty" bson:"question,omitempty"`
 }
 
 type DareContainer []Dare
