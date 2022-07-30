de2a613972e337cfcb990bc41d90561c47a869f3
wpted <silverhandsome@gmail.com>
2022-07-30 11:25:20 +0800

    Create controller SignIn for getting the jwt token

---
+++ controller/userController.go
@@ -0,0 +1,87 @@
+package controller
+
+import (
+	"dareAPI/model"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"net/http"
+	"time"
+)
+
+var MockUser = model.User{
+	UserName: "Admin",
+	Password: "Password",
+}
+var MockSecretKey = "BDABF2B3DF0E000B2C927DCF1E2235320AC3256AB0E97B6CFD390490B43582FA"
+
+type AuthHandler struct{}
+
+// Claims is a set of statement made by the creator to tell info about the subject
+type Claims struct {
+	UserName string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+// JWTOutput is the output format of the combination of a string token according to the payload and the expiration time
+type JWTOutput struct {
+	Token   string
+	Expires time.Time
+}
+
+// SignInHandler is a controller for getting the JWT token
+func (a *AuthHandler) SignInHandler(c *gin.Context) {
+	var user model.User
+
+	// if user input matches the model.user struct, bind the input data to the user variable
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// verify whether the user input is within the database
+	if user.UserName != MockUser.UserName || user.Password != MockUser.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "user not authorized",
+		})
+		return
+	}
+
+	// expirationTime is 10 minutes after the request
+	var expirationTime = time.Now().Add(10 * time.Minute)
+
+	// create a claims according to the user input
+	claims := Claims{
+		UserName: user.UserName,
+		RegisteredClaims: jwt.RegisteredClaims{
+			// NewNumericDate constructs a new *NumericDate from a standard library time.Time struct.
+			// It will truncate the timestamp according to the precision specified in TimePrecision.
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	// NewWithClaims creates a type *Token from the given hashing algorithm and the claims
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		claims,
+	)
+
+	// SignedString is Parsing and Validating the two: the given signature and the signature generated with the secret key
+	tokenString, err := token.SignedString([]byte(MockSecretKey))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	output := JWTOutput{
+		Token:   tokenString,
+		Expires: expirationTime,
+	}
+
+	c.JSON(http.StatusOK, output)
+
+}
